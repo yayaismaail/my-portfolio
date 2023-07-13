2b7714a4030855b897310dbfc0e61c1e4f966a55
ismail halliru muhammad <ismailhm888@gmail.com>
2023-07-13 19:11:16 +0200

    Fix error in menu.js file

--- menu.js
+++ menu.js
@@ -1,14 +1,12 @@
+const mobileMenu = document.querySelector('.mobile-menu');
+const desktopMenu = document.querySelector('.desktop-menu');
 
+mobileMenu.addEventListener('click', () => {
+  mobileMenu.classList.toggle('active');
+  desktopMenu.classList.toggle('active');
+});
 
-const mobileMenu = document.querySelector(".mobile-menu");
-const desktopMenu = document.querySelector(".desktop-menu");
-
-mobileMenu.addEventListener("click", () => {
-    mobileMenu.classList.toggle("active");
-    desktopMenu.classList.toggle("active");
-})
-
-document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
-    mobileMenu.classList.remove("active");
-    desktopMenu.classList.remove("active");
+document.querySelectorAll('.nav-link').forEach((n) => n.addEventListener('click', () => {
+  mobileMenu.classList.remove('active');
+  desktopMenu.classList.remove('active');
 }));
