79a476a5ed659c5516fd536dffad5dd9b0913487
Aisha Alselwi <123330794+AlselwiAisha@users.noreply.github.com>
2023-07-26 16:17:48 +0300

    Update storage.js file

--- storage.js
+++ storage.js
@@ -1,38 +1,27 @@
-// Retrieve the form data from local storage
+const formD = document.querySelector('.form');
+const uName = document.querySelector('#fullname');
+const msg = document.querySelector('#message');
+const emailD = document.querySelector('#email');
+
 function getFormData() {
-  const formData = localStorage.getItem('formData');
-  return formData ? JSON.parse(formData) : {};
+  const formData = {
+    uName: uName.value,
+    emailD: emailD.value,
+    msg: msg.value,
+  };
+  return formData;
 }
 
-// Save the form data to local storage
-function saveFormData(data) {
-  localStorage.setItem('formData', JSON.stringify(data));
-}
-
-// Update the form fields with the saved data
-function updateFormFields() {
+formD.addEventListener('submit', () => {
   const formData = getFormData();
-  document.getElementById('fullname').value = formData.fullname || '';
-  document.getElementById('email').value = formData.email || '';
-  document.getElementById('message').value = formData.message || '';
-}
-
-// Save the form data whenever a field is changed
-function handleInputChange(event) {
-  const inputField = event.target;
-  const fieldName = inputField.fullname;
-  const fieldValue = inputField.value;
-
-  const formData = getFormData();
-  formData[fieldName] = fieldValue;
-  saveFormData(formData);
-}
-
-// Add event listeners to input fields
-const inputFields = document.querySelectorAll('input');
-inputFields.forEach((input) => {
-  input.addEventListener('change', handleInputChange);
+  localStorage.setItem('formData', JSON.stringify(formData));
 });
 
-// Load the form data on page load
-window.addEventListener('load', updateFormFields);
+window.addEventListener('load', () => {
+  const data = JSON.parse(localStorage.getItem('formData'));
+  if (data) {
+    uName.value = data.uName;
+    emailD.value = data.emailD;
+    msg.value = data.msg;
+  }
+});
